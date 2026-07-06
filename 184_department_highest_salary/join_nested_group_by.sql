d52ea84571ec618ccc1679f5b0c39524a5693365
Muhammad Sharjeel Ali <msharjeelali@yahoo.com>
2026-07-06 16:10:30 +0500

    add SQL query to find highest salary by department

---
+++ 184_department_highest_salary/join_nested_group_by.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT D.name AS Department , E.name AS Employee, E.salary AS Salary FROM Employee AS E JOIN Department AS D ON E.departmentId = D.id WHERE (E.departmentId, E.salary) IN (SELECT departmentId, MAX(salary) FROM Employee GROUP BY departmentId);
