b4a4137e1726c19cbbd9a20f26d77178ef7e6a1b
Muhammad Sharjeel Ali <msharjeelali@yahoo.com>
2026-07-06 16:31:44 +0500

    implement brute force solution for covered intervals with improved memory

--- 1288_remove_covered_intervals/brute_force.cpp
+++ 1288_remove_covered_intervals/brute_force.cpp
@@ -20,3 +20,26 @@ public:
        return result.size();
     }
 };
+
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        int count = 0;
+
+       for(int i = 0; i < intervals.size(); i++){
+        bool isCovered = false;
+        for(int j = 0; j < intervals.size() && !isCovered; j++){
+            if(i == j){
+                continue;
+            }
+            if(intervals[i][0] >= intervals[j][0] && intervals[i][1] <= intervals[j][1]){
+                isCovered = true;
+            }
+        }
+        if(!isCovered){
+            count++;
+        }
+       }
+       return count;
+    }
+};
